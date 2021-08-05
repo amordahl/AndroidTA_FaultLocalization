e673459cc4a6798172dfc357752ebcc637bdc759
pancax <dakotasoles@gmail.com>
2021-08-05 02:16:02 -0500

    Fixed file path issue

--- resources/delta_debugger/ViolationDeltaDebugger/src/main/java/cs/utd/soles/Runner.java
+++ resources/delta_debugger/ViolationDeltaDebugger/src/main/java/cs/utd/soles/Runner.java
@@ -109,7 +109,7 @@ public class Runner {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-            
+
 
 
         //Before we start delta debugging lets, work on finding the best files to deal with
@@ -698,7 +698,7 @@ public class Runner {
         projectAPKPath=pathFile+"/app/build/outputs/apk/debug/app-debug.apk";
         projectSrcPath=pathFile+"/app/src/";
         dealWithSpecialProjects(APKName, pathFile);
-        projectClassFiles = pathFile+"/"+projectAPKPath.substring(1,projectAPKPath.indexOf("/",1))+"/build/intermediates/javac/debug/classes";
+        projectClassFiles = pathFile+"/"+projectAPKPath.substring(1,projectAPKPath.indexOf("/",pathFile.length()+1))+"/build/intermediates/javac/debug/classes";
     }
 
     private static void dealWithSpecialProjects(String name, String pathFile) {
