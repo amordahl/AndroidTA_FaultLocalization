d8a22e2c5f2e93343e285ab9a09dd8692ac66d08
Austin Mordahl <austin.mordahL@utdallas.edu>
2021-10-20 17:10:09 -0500

    updated coverage wrapper script

--- instrumentation/resources/coverage_wrapper.py
+++ instrumentation/resources/coverage_wrapper.py
@@ -6,20 +6,22 @@ import argparse
 p = argparse.ArgumentParser()
 p.add_argument('apk')
 p.add_argument('-c', '--config',
-               help='IMPORTANT: Enclose the configuration in double quotes.')
+               help='IMPORTANT: Enclose the configuration in double quotes.', default = "")
 p.add_argument('-f', '--force', action='store_true',
                help='Redo experiments that are already done. Experiments that '
                'are done are determined by checking for existing output logs.')
 p.add_argument('--datadir', help='Where to put the logs.', default='.')
+p.add_argument('--phase2log', help='A coverage log that triggers phase 2 instrumentation.')
+p.add_argument('--suffix', help='File extension.', default = '.instlog')
 args = p.parse_args()
 
 
 
 def main():
-    CMD = "java -javaagent:/home/asm140830/.m2/repository/edu/utdallas/amordahl/Coverage_Instrumenter/1.0.0-SNAPSHOT/Coverage_Instrumenter-1.0.0-SNAPSHOT.jar -classpath /home/asm140830/.m2/repository/edu/utdallas/amordahl/Coverage_Instrumenter/1.0.0-SNAPSHOT/Coverage_Instrumenter-1.0.0-SNAPSHOT.jar:/home/asm140830/.m2/repository/edu/utdallas/amordahl/FL_Logger/1.0-SNAPSHOT/FL_Logger-1.0-SNAPSHOT.jar -jar soot-infoflow-cmd-jar-with-dependencies.jar -a {apk} -p /home/asm140830/AndroidTA/Android/platforms -s /home/asm140830/AndroidTA/AndroidTAEnvironment/tools/FlowDroid/soot-infoflow-android/SourcesAndSinks.txt {config}"
+    CMD = "java -javaagent:/home/asm140830/.m2/repository/edu/utdallas/amordahl/MultiPhaseInstrumenter/1.0.0/MultiPhaseInstrumenter-1.0.0.jar{phase2log} -classpath /home/asm140830/.m2/repository/edu/utdallas/amordahl/MultiPhaseInstrumenter/1.0.0/MultiPhaseInstrumenter-1.0.0.jar:/home/asm140830/.m2/repository/edu/utdallas/amordahl/FL_Logger/1.0/FL_Logger-1.0.jar -jar soot-infoflow-cmd-jar-with-dependencies.jar -a {apk} -p /home/asm140830/AndroidTA/Android/platforms -s /home/asm140830/AndroidTA/AndroidTAEnvironment/tools/FlowDroid/soot-infoflow-android/SourcesAndSinks.txt {config}"
     
     # Replace strings in cmd.
-    base_apk : str = os.path.basename(args.apk).replace('.apk', '')
+    base_apk : str = os.path.basename(args.apk).replace('.apk', '').replace('_', '')
     CMD = CMD.replace('{apk}', args.apk)
     logging.debug(f'args.config is {args.config}')
     config = args.config.replace('"', '')
@@ -28,7 +30,9 @@ def main():
     if output_file[-1] == '_':
         output_file = output_file[0:-2]
 
-    output_file = output_file + '.instlog'
+    CMD = CMD.replace('{phase2log}', f'=coverage,{args.phase2log}' if args.phase2log else '')
+    
+    output_file = output_file + args.suffix
     output_file = os.path.join(args.datadir, output_file)
 
     CMD = CMD.replace('{config}', config if config != None else '')
